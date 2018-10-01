232961b7bfa00f009b9dcbf78be3555a1e8e5567
IMRON REVIADY <imronreviady2@gmail.com>
2018-10-02 04:51:02 +0700

    Create server models and controllers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
 ReactDOM.render(
 	<div>Hola</div>
 	, document.getElementById('root'));
